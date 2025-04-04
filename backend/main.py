3dc500f95d033dffb98db847d53e399a860df450
MarzanIvan <boss.marzan@bk.ru>
2025-04-04 19:49:02 +0300

    add new origins to cors

--- backend/main.py
+++ backend/main.py
@@ -37,7 +37,10 @@ app.mount("/counter", counter_app)
 # Настройка CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://89.111.169.18"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
