cba37afcaae06d62cc0fbed2a2708bb9016b7d81
MarzanIvan <boss.marzan@bk.ru>
2025-04-17 22:08:27 +0300

    add auth.py

--- backend/main.py
+++ backend/main.py
@@ -7,11 +7,13 @@ from fastapi import FastAPI, HTTPException, Form, BackgroundTasks, WebSocket, We
 from yt_dlp import YoutubeDL
 from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime
-
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import time
+import pickle
 
 # Импортируем компонент счётчика
 from counter import counter_app
-from auth import auth_app
 
 COOKIES_PATH = "./app/cookies.txt/cookies.txt"
 
@@ -35,9 +37,6 @@ os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 # Создаём FastAPI приложение
 app = FastAPI()
 app.mount("/counter", counter_app)
-
-authapp = FastAPI()
-authapp.mount("/auth", auth)
 
 # Настройка CORS
 app.add_middleware(
@@ -103,12 +102,43 @@ async def websocket_endpoint(websocket: WebSocket):
         logger.info("WebSocket disconnected")
 
 
-@authapp.post("/auth/")
-async def auth_youtube(email: str = Form(...), password: str = Form(...)):
-    success = login_and_save_cookies(email, password)
-    if success:
-        return {"message": "Авторизация прошла успешно и куки сохранены."}
-    raise HTTPException(status_code=401, detail="Не удалось авторизоваться.")
+def login_and_save_cookies(email: str, password: str):
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+
+    driver = webdriver.Firefox(options=options)
+    try:
+        driver.get("https://accounts.google.com/signin/v2/identifier?service=youtube")
+
+        time.sleep(2)
+        driver.find_element("id", "identifierId").send_keys(email)
+        driver.find_element("id", "identifierNext").click()
+
+        time.sleep(3)
+        driver.find_element("name", "password").send_keys(password)
+        driver.find_element("id", "passwordNext").click()
+
+        time.sleep(10)  # Подожди пока загрузится
+
+        os.makedirs(os.path.dirname(COOKIES_PATH), exist_ok=True)
+        with open(COOKIES_PATH, "wb") as file:
+            pickle.dump(driver.get_cookies(), file)
+
+        return True
+    except Exception as e:
+        print("Ошибка входа:", e)
+        return False
+    finally:
+        driver.quit()
+
+def load_cookies_to_browser(driver):
+    if not os.path.exists(COOKIES_PATH):
+        return
+    with open(COOKIES_PATH, "rb") as file:
+        cookies = pickle.load(file)
+        for cookie in cookies:
+            driver.add_cookie(cookie)
 
 @app.post("/download_video/")
 async def download_video(
